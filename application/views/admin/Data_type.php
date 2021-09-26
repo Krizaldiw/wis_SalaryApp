06707e9449b1344cedd4e04565bd62c5a4725416
Khalif Rizaldi <khalifrizaldi04@gmail.com>
2021-09-26 09:43:50 +0700

    Menambahkan commit file

---
+++ application/views/admin/Data_type.php
@@ -0,0 +1,29 @@
+<div class="main-content">
+  <div class="section">
+    <div class="section-header">
+      <h1>Data Type Mobil</h1>
+    </div>
+  </div>
+  <a href="<?php echo base_url('admin/data_type/tambah_type')?>" class="btn btn-primary mb-4"><i class="fas fa-plus fa-sm"></i> Tambah Type</a>
+  <?php echo $this->session->flashdata('pesan') ?>
+  <table class="table table-bordered table-striped table-hover">
+    <thead>
+      <tr>
+        <th width="20px">No</th>
+        <th>Kode Type</th>
+        <th>Nama Type</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $no = 1;
+      foreach($type as $tp):  ?>
+        <tr>
+          <td></<?php echo $no++ ?></td>
+          <td></<?php echo $tp->kode_type ?></td>
+          <td></<?php echo $tp->nama_type ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+</table>
+</div>
---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,6 @@
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Dashboard</h1>
+          </div>
